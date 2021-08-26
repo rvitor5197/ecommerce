67299e4e37b0900802909fb76b757aefe9a2d550
Vitor Fonseca <rvitor5197@gmail.com>
2021-08-26 14:13:15 -0300

    login admin

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
-
+error_reporting(E_ALL ^ E_DEPRECATED);
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -18,11 +20,41 @@ $app->get('/', function() {
 });
 
 $app->get('/admin', function() {
+
+	User::verifyLogin();
     
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
 });
+
+$app->get('/admin/login', function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function(){
+	User::logout();
+
+	header("Location: /login");
+	exit;
+})
+
 
 $app->run();
 
